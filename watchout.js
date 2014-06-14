1cc55fdb474e0e15562ef891de649cf539fa22a6
Yahkob <jakobharclerode@gmail.com>
2014-06-14 15:55:49 -0700

    Enemies are now David Hasselhoff and added bootbox

--- watchout.js
+++ watchout.js
@@ -1,3 +1,13 @@
+$(document).ready(function(){
+bootbox.confirm("Are you sure you wont cry when the hoff hassels you??", function(result) {
+  if(result){
+    bootbox.hideAll();
+  }
+  else{
+    window.location.href="http://tucsoncitizen.com/morgue/files/2007/01/l38304-1.jpg"
+  }
+});
+})
 var gameSettings = {
   height: 600,
   width: 700,
@@ -40,12 +50,14 @@ var enemies = makeEnemies();
 var board = d3.select('.container').append('svg')
                 .attr({width: gameSettings.width,height: gameSettings.height})
                 .attr({class: 'board'});
-var enemy = board.selectAll('circle').data(enemies).enter().append('circle')
+var enemy = board.selectAll('image').data(enemies).enter().append('svg:image')
 .attr("r",10 )
-.attr("fill",'teal')
-.attr('cx',function(i){return i.cX})
+.attr("xlink:href",'dave.png')
+.attr('x',function(i){return i.cX})
 .attr('id',function(i){return i.id})
-.attr('cy',function(i){return i.cY});
+.attr('width', 50)
+.attr('height',90)
+.attr('y',function(i){return i.cY});
 
 
 
@@ -63,15 +75,16 @@ var player = board.selectAll('player').data([666]).enter().append('circle')
 .attr('cx',350)
 .attr('class','player').call(drag)
 
+
 setInterval(function(){
-  enemy.transition().duration(1900).attr('cx',function(i){return Math.random() * 900})
-  .attr('cy',function(){return Math.random() * 750})
+  enemy.transition().duration(1900).attr('x',function(i){return Math.random() * 900})
+  .attr('y',function(){return Math.random() * 750})
   .tween('custom', function(i,d){return function(t){
     var enemy = d3.select(this);
     var radiusSum =  parseFloat(enemy.attr('r')) + d3.select('.player').attr('r')
     var hero = d3.select('.player');
-    var yDiff = parseFloat(enemy.attr('cy')) - parseFloat(hero.attr('cy'));
-    var xDiff = parseFloat(enemy.attr('cx')) - parseFloat(hero.attr('cx'));
+    var yDiff = parseFloat(enemy.attr('y')) - parseFloat(hero.attr('cy'));
+    var xDiff = parseFloat(enemy.attr('x')) - parseFloat(hero.attr('cx'));
     var separation = Math.sqrt( Math.pow(xDiff,2) + Math.pow(yDiff,2) )
     if(separation < 30){
       if(score.highScore < score.currentScore){
