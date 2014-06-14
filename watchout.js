81abe0f31fdcf8c6ccb12eff8ffb64fd2bb2768c
Yahkob <jakobharclerode@gmail.com>
2014-06-14 16:49:21 -0700

    code refactoring and added a skin for player

--- watchout.js
+++ watchout.js
@@ -1,13 +1,14 @@
 $(document).ready(function(){
-bootbox.confirm("Are you sure you wont cry when the hoff hassels you??", function(result) {
-  if(result){
-    bootbox.hideAll();
-  }
-  else{
-    window.location.href="http://tucsoncitizen.com/morgue/files/2007/01/l38304-1.jpg"
-  }
+  bootbox.confirm("Are you sure you wont cry when the hoff hassels you??", function(result) {
+    if(result){
+      bootbox.hideAll();
+    }
+    else{
+      window.location.href="http://tucsoncitizen.com/morgue/files/2007/01/l38304-1.jpg"
+    }
+  });
 });
-})
+
 var gameSettings = {
   height: 600,
   width: 700,
@@ -43,15 +44,12 @@ var makeEnemies = function() {
   });
 };
 
-
-
-
 var enemies = makeEnemies();
+
 var board = d3.select('.container').append('svg')
-                .attr({width: gameSettings.width,height: gameSettings.height})
-                .attr({class: 'board'});
+.attr({width: gameSettings.width,height: gameSettings.height})
+.attr({class: 'board'});
 var enemy = board.selectAll('image').data(enemies).enter().append('svg:image')
-.attr("r",10 )
 .attr("xlink:href",'dave.png')
 .attr('x',function(i){return i.cX})
 .attr('id',function(i){return i.id})
@@ -62,48 +60,46 @@ var enemy = board.selectAll('image').data(enemies).enter().append('svg:image')
 
 
 var drag = d3.behavior.drag()
-    .on("drag", function(d){
-     d3.select(this)
-      .attr("cx", d.x = Math.max(10, Math.min(690, d3.event.x)))
-      .attr("cy", d.y = Math.max(10, Math.min(590, d3.event.y)));
-})
+.on("drag", function(d){
+  d3.select(this)
+  .attr("x", d.x = Math.max(10, Math.min(690, d3.event.x)))
+  .attr("y", d.y = Math.max(10, Math.min(590, d3.event.y)));
+});
 
-var player = board.selectAll('player').data([666]).enter().append('circle')
-.attr('r',13)
-.attr('fill','magenta')
-.attr('cy',200)
-.attr('cx',350)
-.attr('class','player').call(drag)
-
+var player = board.selectAll('player').data([666]).enter().append('svg:image')
+.attr("xlink:href", "Sale_1.png")
+.attr('width', 73)
+.attr('height', 120)
+.attr('y',200)
+.attr('x',350)
+.attr('class','player').call(drag);
 
 setInterval(function(){
   enemy.transition().duration(1900).attr('x',function(i){return Math.random() * 900})
   .attr('y',function(){return Math.random() * 750})
-  .tween('custom', function(i,d){return function(t){
-    var enemy = d3.select(this);
-    var radiusSum =  parseFloat(enemy.attr('r')) + d3.select('.player').attr('r')
-    var hero = d3.select('.player');
-    var yDiff = parseFloat(enemy.attr('y')) - parseFloat(hero.attr('cy'));
-    var xDiff = parseFloat(enemy.attr('x')) - parseFloat(hero.attr('cx'));
-    var separation = Math.sqrt( Math.pow(xDiff,2) + Math.pow(yDiff,2) )
-    if(separation < 30){
-      if(score.highScore < score.currentScore){
-        score.highScore = 0 + score.currentScore;
+  .tween('custom', function(i,d){
+    return function(t){
+      var enemy = d3.select(this);
+      var hero = d3.select('.player');
+      var yDiff = parseFloat(enemy.attr('y')) - parseFloat(hero.attr('y'));
+      var xDiff = parseFloat(enemy.attr('x')) - parseFloat(hero.attr('x'));
+      var separation = Math.sqrt( Math.pow(xDiff,2) + Math.pow(yDiff,2))
+      if(separation < 30){
+        if(score.highScore < score.currentScore){
+          score.highScore = 0 + score.currentScore;
+        }
+        score.collisions += 1;
+        score.currentScore = 0;
       }
-      score.collisions += 1
-      score.currentScore = 0;
     }
-  }
   });
 },time);
 
 function increaseScore(){
-  score.currentScore += 2
-  updateScore()
-}
+  score.currentScore += 2;
+  updateScore();
+};
 
 setInterval(function(){
-  increaseScore()
-},50)
-updateScore()
-
+  increaseScore();
+},50);
